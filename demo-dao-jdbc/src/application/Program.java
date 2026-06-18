670f23032689c9d63d48137fe9ad99aa656b22f2
Alberes <marcus.alberes@gmail.com>
2026-06-18 16:11:17 -0300

    SellerDAO Completo

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,24 +1,18 @@
 package application;
 
-import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
-import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 	
+		
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();	
-		Seller seller = sellerDao.findById(3);
-		System.out.println(seller);
+		sellerDao.deleteById(20);
 		
-		
-		Department department = new Department(1, null);
-		List<Seller> list = sellerDao.findByDepartment(department);
-		list.forEach(System.out::println);
 		
 	}
 
