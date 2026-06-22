211be72d90086793761a4c9a494a44f06511694d
Alberes <marcus.alberes@gmail.com>
2026-06-22 08:20:21 -0300

    Projeto DAO finalizado

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -2,18 +2,27 @@ package application;
 
 
 import model.dao.DaoFactory;
-import model.dao.SellerDao;
+import model.dao.DepartmentDao;
 
 public class Program {
 
 	public static void main(String[] args) {
 	
+		DepartmentDao dep = DaoFactory.createDepartmentDao();
+
+
+		/*
+		System.out.println(dep.findById(5));
 		
+		List<Department> list = new ArrayList<>();
+		list = dep.findAll();
+		list.forEach(System.out::println);
 		
-		SellerDao sellerDao = DaoFactory.createSellerDao();	
-		sellerDao.deleteById(20);
+		Department depart1 = new Department(null, "RH");
+		dep.insert(depart1);
 		
-		
+		dep.deleteById(5);
+		*/
 	}
 
 }
